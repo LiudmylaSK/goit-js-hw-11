6c8e86fbfd4de81714300c1b9494e45686f79fc5
LiudmylaSK <129587382+LiudmylaSK@users.noreply.github.com>
2023-09-15 12:56:16 +0000

    Deploying to gh-pages from @ LiudmylaSK/goit-js-hw-11@8a506dfa0757e1d9147871aa128b9782becfe83b 🚀

--- index.477b75c2.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=document.getElementById("scrollToTopBtn");o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})})),window.addEventListener("scroll",(function(){window.scrollY>300?o.style.display="block":o.style.display="none"}))}();
-//# sourceMappingURL=index.477b75c2.js.map
--- index.c0f7fb05.js
+++
@@ -1,2 +0,0 @@
-const o=document.getElementById("scrollToTopBtn");o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})})),window.addEventListener("scroll",(function(){window.scrollY>300?o.style.display="block":o.style.display="none"}));
-//# sourceMappingURL=index.c0f7fb05.js.map
